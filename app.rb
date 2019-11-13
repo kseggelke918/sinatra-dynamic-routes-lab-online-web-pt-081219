bb7fc87dea8c81ae1f41c8cec28ea909208c4afd
Katie Seggelke <katie.seggelke@gmail.com>
2019-11-13 22:40:05 +0000

    Automatically backed up by Learn

--- app.rb
+++ app.rb
@@ -39,7 +39,7 @@ class App < Sinatra::Base
     elsif @operation == "subtract"
       @number1 = @number2
     elsif @operation == "multiply"
-      @number1 * @number2
+      #{@number1} * #{@number2}
     else
       @number1 / @number2
     end 
