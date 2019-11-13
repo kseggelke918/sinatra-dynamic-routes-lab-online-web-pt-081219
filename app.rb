7fe17e9b9958ed06e94d38ab0994341f641cf015
Katie Seggelke <katie.seggelke@gmail.com>
2019-11-13 22:41:50 +0000

    Automatically backed up by Learn

--- app.rb
+++ app.rb
@@ -40,8 +40,10 @@ class App < Sinatra::Base
       (@number1 = @number2).to_s 
     elsif @operation == "multiply"
       (@number1 * @number2).to_s 
-    else
+    elsif @operation == "divide"
       (@number1 / @number2).to_s 
+    else 
+      "I didn't get that"
     end 
       
 
